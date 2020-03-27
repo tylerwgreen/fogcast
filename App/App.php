453cea1b345e0c0afda371c614b0c5a112e46b72
tylerwgreen <freshgreenmedia@gmail.com>
2020-03-26 19:02:47 -0700

    added maps and fixed css

--- App/App.php
+++ App/App.php
@@ -1,8 +1,17 @@
 <?php
 // load dependencies
-define('BASE_DIR', dirname(__FILE__) . '/');
-require(BASE_DIR . 'functions.php');
-require(BASE_DIR . 'WeatherApi/WeatherApi.php');
+define('DIR_BASE', dirname(dirname(__FILE__)) . '/');
+define('DIR_APP', DIR_BASE . 'app/');
+require(DIR_APP . 'functions.php');
+require(DIR_APP . 'WeatherApi/WeatherApi.php');
 
 // define app vars
-define('BASE_URL', baseUrl());
+define('DIR_DATA', DIR_BASE		. 'data/');
+
+define('URL_BASE', baseUrl());
+define('URL_IMG', baseUrl()		. 'img/');
+define('URL_CSS', baseUrl()		. 'css/');
+define('URL_LESS', baseUrl()	. 'less/');
+define('URL_JS', baseUrl()		. 'js/');
+
+// define('CACHE_WEATHER', true);
--- index.php
+++ index.php
@@ -6,6 +6,14 @@ $zonesCombined = [
 	// 'ID',
 ];
 foreach($zonesCombined as $k => $v){
+	// $cacheFile = DIR_DATA . $v . '.json';
+	// if(CACHE_WEATHER_READ){
+		// $zone = json_decode(file_get_contents($cacheFile));
+	// }else{
+		// $zone = new Zones($v);
+		// if(CACHE_WEATHER)
+			// file_put_contents($cacheFile, json_encode($zone));
+	// }
 	$zonesCombined[$k] = new Zones($v);
 }
 ?><!doctype html>
@@ -15,8 +23,10 @@ foreach($zonesCombined as $k => $v){
 	<title>Fogcast</title>
 	<meta name="description" content="Fogcast">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="<?= URL_CSS; ?>style.css">
+	<!--
+	<link rel="stylesheet/less" type="text/css" href="<?= URL_LESS; ?>style.less" />
+	-->
 </head>
 <body>
 	<table>
@@ -26,28 +36,70 @@ foreach($zonesCombined as $k => $v){
 					$forecast = $zone->getForecast();
 				?>
 					<tr class="<?= $alt ? 'alt' : ''; ?>">
-						<td class="overview">
-							<span class="<?= $zone->hasRain ? 'zone-has-rain' : ''; ?>">Rain</span>
-							<span class="<?= $zone->hasThunder ? 'zone-has-thunder' : ''; ?>">Thunder</span>
-							<span class="<?= $zone->hasSnow ? 'zone-has-snow' : ''; ?>">Snow</span>
-							<span class="<?= $zone->hasFog ? 'zone-has-fog' : ''; ?>">Fog</span>
+						<td class="map-tile-cell">
+							<div class="map-tile-wrap">
+								<img src="<?= URL_IMG  . '/map/tiles/' . $zone->feature->properties->id; ?>.png" alt="map tile"/>
+							</div>
 						</td>
-						<td><a
-							href="https://forecast.weather.gov/MapClick.php?w0=t&w1=td&w2=wc&w3=sfcwind&w3u=1&w4=sky&w5=pop&w6=rh&w7=rain&w8=thunder&w9=snow&w10=fzg&w11=sleet&w12=fog&w13u=0&w16u=1&AheadHour=0&Submit=Submit&FcstType=graphical&textField1=<?= $zone->geometryCoordinatesCentral->x; ?>&textField2=<?= $zone->geometryCoordinatesCentral->y; ?>&site=all&unit=0&dd=&bw="
-							target="_blank"
-							><?= $zone->feature->properties->id; ?><br/>Hourly Forecast</a></td>
-						<td><a
-							href="https://www.google.com/maps/search/@<?= $zone->geometryCoordinatesCentral->x; ?>,<?= $zone->geometryCoordinatesCentral->y; ?>,12z"
-							target="_blank"
-							><?= $zone->feature->properties->name; ?><br/>Map</a></td>
-						<td class="updated"><?= date('y-m-d\<\b\r\/\>H:i:s', $forecast->updated); ?></td>
+						<td class="meta-cell">
+							<div class="overview">
+								<span class="<?= $zone->hasRain ? 'zone-has-rain' : ''; ?>">Rain</span>
+								<span class="<?= $zone->hasThunder ? 'zone-has-thunder' : ''; ?>">Thunder</span>
+								<span class="<?= $zone->hasSnow ? 'zone-has-snow' : ''; ?>">Snow</span>
+								<span class="<?= $zone->hasFog ? 'zone-has-fog' : ''; ?>">Fog</span>
+							</div>
+							<div class="hourly-forecast-link">
+								<a
+									href="https://forecast.weather.gov/MapClick.php?w0=t&w1=td&w2=wc&w3=sfcwind&w3u=1&w4=sky&w5=pop&w6=rh&w7=rain&w8=thunder&w9=snow&w10=fzg&w11=sleet&w12=fog&w13u=0&w16u=1&AheadHour=0&Submit=Submit&FcstType=graphical&textField1=<?= $zone->geometryCoordinatesCentral->x; ?>&textField2=<?= $zone->geometryCoordinatesCentral->y; ?>&site=all&unit=0&dd=&bw="
+									target="_blank"
+									><?= $zone->feature->properties->id; ?><br/>Hourly Forecast</a>
+							</div>
+							<div class="map-link">
+								<a
+									href="https://www.google.com/maps/search/@<?= $zone->geometryCoordinatesCentral->x; ?>,<?= $zone->geometryCoordinatesCentral->y; ?>,12z"
+									target="_blank"
+									><?= $zone->feature->properties->name; ?><br/>Map</a>
+							</div>
+							<div class="updated">
+								<?= date('y-m-d\<\b\r\/\>H:i:s', $forecast->updated); ?>
+							</div>
+						</td>
 						<?php foreach($forecast->periods as $period): ?>
-							<td class="forecast <?= $period->rain ? 'period-has-rain' : ''; ?> <?= $period->thunder ? 'period-has-thunder' : ''; ?> <?= $period->snow ? 'period-has-snow' : ''; ?> <?= $period->fog ? 'period-has-fog' : ''; ?>"><b><?= $period->name; ?>:</b> <?= $period->forecast; ?></td>
+							<td class="forecast-cell">
+								<div class="forecast-wrap">
+									<div class="forecast-types">
+										<span class="rain <?= $period->rain ? 'period-has-rain' : ''; ?>">Rain</span>
+										<span class="thunder <?= $period->thunder ? 'period-has-thunder' : ''; ?>">Thunder</span>
+										<span class="snow <?= $period->snow ? 'period-has-snow' : ''; ?>">Snow</span>
+										<span class="fog <?= $period->fog ? 'period-has-fog' : ''; ?>">Fog</span>
+									</div>
+									<div class="forecast">
+										<b><?= $period->name; ?>:</b> <?= $period->forecast; ?>
+									</div>
+								</div>
+							</td>
 						<?php endforeach; ?>
 					</tr>
 				<?php $alt = $alt ? false :true; endforeach; ?>
 			<?php endforeach; ?>
 		</tbody>
 	</table>
+	<!--
+	<script>
+		less = {
+			env: "development",
+			async: false,
+			fileAsync: false,
+			poll: 100,
+			functions: {},
+			dumpLineNumbers: "comments",
+			relativeUrls: false,
+			rootpath: ":/a.com/"
+		};
+	</script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script>
+	<script>less.watch();</script>
+	<script src="<?= URL_JS; ?>script.js"></script>
+	-->
 </body>
 </html>
